5ed704fb7640ab45c2fbfe9359d5475d904d3e4d
George Assaad <lopgogo@gmail.com>
2022-07-08 00:35:38 +0200

    ex 3.3

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ app.get('/info', (request, response) => {
     response.send(`<div>Phonebook has info for ${persons.length} people</div> <br> <div>${(new Date())}</div> `)
 })
 
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) { 
+        response.json(person)  
+    } else { 
+        response.status(404).end()  
+    }
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
