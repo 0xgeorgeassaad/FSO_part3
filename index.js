ece10905183c741c01609a9b19a95480ab7912f1
George Assaad <lopgogo@gmail.com>
2022-07-08 01:21:55 +0200

    ex 3.6

--- index.js
+++ index.js
@@ -61,10 +61,14 @@ app.delete('/api/persons/:id', (request, response) => {
   
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (!body) {
+    if (!body.name || !body.number ) {
       return response.status(400).json({ 
-        error: 'body missing' 
+        error: 'Either name or number is missing' 
       })
+    }else if(persons.find(person=> person.name.toLowerCase() == body.name.toLowerCase())){
+        return response.status(400).json({ 
+            error: `${body.name} already in phonebook.` 
+        })
     }
     const person = {
       ...body,
